4be368f764de316d831c39fde63a664539739082
ryukinix <manoel_vilela@engineer.com>
2015-06-04 01:04:32 -0300

    The prototype algorithm to develop in C ANSI

---
+++ C/sqrt_aprox.c
@@ -0,0 +1,65 @@
+/*
+ * Presentes:
+ * 1. Manoel Vilela 
+ * 2.
+ * 3.
+ * 4.
+ * 5.
+ * 6.
+ * 
+ *
+ * Fixing:
+ *
+ *
+ *
+ *
+ *
+ *
+ * Issues:
+ *
+ *
+ *
+ *
+ *
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * Função refactor:
+ * Objetivo -> Refatorar raiz quadrada como (z)^(1/m) -> k(1 + x)^1/m |
+ *
+ *  			-*- Desenvolvimento -*-
+ *
+ * Seja z = 18 e m  = 2; Logo -> sqrt(18) = sqrt(2 * 9) = 3sqrt(2) = 3sqrt(1 + 1);
+ * ou seja: se z = 18 e m = 2, k = 3 e x = 1. 
+ *
+**/
+
+void refactor(int *in_sqrt, int *out_sqrt){
+ 	 
+
+  
+  
+  
+  
+}
+
+
+//função final
+double sqrt_(double z){
+  
+  
+  
+  return ;
+}
+
+void menu(void){
+  
+}
+
+int main(int argc, char *argv[]){
+	printf("Projeto de Engenharia - Pat T");
+}
