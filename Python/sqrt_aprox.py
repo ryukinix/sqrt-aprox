249d2d031f94762b26a537504aab5a38a254740a
Manoel Vilela <manoel_vilela@engineer.com>
2015-06-05 09:02:47 -0300

    New entry for new tests (i must have generalize that code for future :p )

--- Python/sqrt_aprox.py
+++ Python/sqrt_aprox.py
@@ -10,8 +10,8 @@ p_impar = lambda x: reduce(lambda a, b: a*b, (abs(2*k + 1) for k in range(-1, x-
 #aproximação da raiz quadrada usando expansão da série de taylor
 expr1 = lambda x, n: float((((-1)**(int(n)+1))*((x - 1)**n)*p_impar(int(n)))/((2.0**n)*f(int(n))))  #preciso revisar, alguma expressão estar errada
 
-sqrt_ = lambda x: 1 + sum([expr1(float(x), float(n)) for n in range(1, 150)]) #funciona que é uma maravilha para o número 2.
-i = 50
+sqrt_ = lambda x: 1 + sum([expr1(float(x), float(n)) for n in range(1, 7)]) #funciona que é uma maravilha para o número 2.
+i = 2
 print("my: √%d = %2.10f" %(i, sqrt_(i)))
 print("python: √%d = %2.10f" %(i, sqrt(i)))
 print("sqrt - sqrt_ = %2.10f" %(abs(sqrt(i) - sqrt_(i))))
