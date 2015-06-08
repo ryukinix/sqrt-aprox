ecafc41c114981cc3f257d85a6065bf3174b1d2e
Manoel Vilela <manoel_vilela@engineer.com>
2015-06-08 00:49:20 -0300

    Increment some comments!

--- C/make-compile.sh
+++ C/make-compile.sh
@@ -4,7 +4,7 @@
 # This script compile all the files .c to a object-code and after link in a executable file work.out  
 #
 
-#function to compile
+#function to compile in object-code
 compile(){
 	for file in ./*.c
 	do
@@ -12,12 +12,12 @@ compile(){
 	done
 }
 
+#function to link the object-codes and your libraries to a executable
 link (){
 	gcc ./*.o -lm -o work.out
 }
 
 #execution compile
-
 compile
 
 if [ $? -eq 0 ];
